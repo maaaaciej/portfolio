6fd1af077033cadd5e7ca2b97fd8470206526f01
Maciej <ostromaciej@gmail.com>
2020-07-05 01:30:16 +0200

    portfolio v2 with new projecs and sourcecode instead of only the build

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -35,9 +35,9 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <p className="hero-cta">
             <span className="cta-btn cta-btn--hero">
-              <Link to="about" smooth duration={1000}>
+              <a href="https://github.com/maaaaciej" target="_blank" smooth duration={1000}>
                 {cta || 'Know more'}
-              </Link>
+              </a>
             </span>
           </p>
         </Fade>
--- src/mock/data.js
+++ src/mock/data.js
@@ -2,93 +2,111 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Maciej Ostrowski | Web Developer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'A showcase of my work.', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: "Hi! I'm",
+  name: 'Maciej Ostrowski',
+  subtitle: "I'm a web developer",
+  cta: 'Check out my GitHub!',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'profil.png',
+  paragraphOne: "Hi! I'm Maciej, an Oslo-based front end developer!",
+  paragraphTwo: "I've been a tech-head for all my life, dissecting any electronic device I could land my hands on, to see how it worked. This passion transfered over to software, and I started coding at 11, trying to deconstruct webpages to see how they worked.",
+  paragraphThree: "I specialize in HTML, CSS and JavaScript, React and Redux, and I pick up any new libraries easily.",
+  resume: 'https://www.dropbox.com/s/7nblds9gxuuhr03/MaciejOstrowskiResume.pdf?dl=0', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'ecommerce.png',
+    title: 'Ecommerce Showcase',
+    info: "An online store made using react. Users are able to log in, add items to the cart, browse different sections and check out with stripe.",
+    info2: 'I used redux for state management, firebase for google sign-in and storing users. "Payments" are processed with the stripe api',
+    url: 'http://maciejclothing.herokuapp.com',
+    repo: 'https://github.com/maaaaciej/ecommerce', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'dadjoke.png',
+    title: 'Dad Joke API',
+    info: 'A dad joke website, that returns a random dad joke each time you access it. I have also made an API to go with it, so that anyone can make their own version of it.',
+    info2: 'Made in python with flask.',
+    url: 'https://dadjokeapi.herokuapp.com',
+    repo: 'https://github.com/maaaaciej/dadjokeapi', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'cats.png',
+    title: 'Cat Facts',
+    info: 'A React app made using the Cat Facts Api. The app displays the fact from the JSON response from the api. A new response happens on every button click, and the fact is passed down to the text component with state.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://maaaaciej.github.io/catfacts/',
+    repo: 'https://github.com/maaaaciej/catfacts', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'pics.png',
+    title: 'Unsplash API Search Engine',
+    info: 'An image search engine made using React and the unsplash API. The API requests are made using Axios.',
+    info2: 'The images are placed in a responsive CSS grid, where each image takes up a number of rows relative to the image height.',
+    url: 'https://maaaaciej.github.io/pics/',
+    repo: 'https://github.com/maaaaciej/pics', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'mimeleken.png',
+    title: 'Mimeleken',
+    info: 'Mimeleken, the norwegian version of charades, is a fun game for pretty much anyone. I made this app using vanilla JS.',
+    info2: 'It was developed with mobile users in mind, as a game like this is best suited on a phone.',
+    url: 'https://maaaaciej.github.io/mimeleken/',
+    repo: 'https://github.com/maaaaciej/mimeleken', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'color.png',
+    title: 'RGB Value Guessing Game',
+    info: 'A vanilla JS color guessing game. You get a RGB value on top, and have to click the correctly colored div to win.',
+    info2: 'I use this website often when working with projects, to get some inspiration on new colors, as well as getting some training in guessing RGB values.',
+    url: 'https://maaaaciej.github.io/color-picker/',
+    repo: 'https://github.com/maaaaciej/color-picker', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'If you want to get in touch, talk to me about a project collaboration or just say hi, click the button down below, or send an email to ostromaciej@gmail.com!',
+  btn: "Let's talk!",
+  email: 'ostromaciej@gmail.com',
 };
 
 // FOOTER DATA
 export const footerData = {
   networks: [
-    {
-      id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
+
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/maciej-ostrowski-038b53138/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/maaaaciej',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
